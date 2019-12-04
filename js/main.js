b39db6bca14e62d88610d19631432e7359ebb6c0
Aliaksei <alv96@bk.ru>
2019-12-04 23:03:22 +0300

    fix: update files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        cssEase: 'linear'
+    });
+});

38a272f41f999f975e65e1ecb6b804ef6f7d10f7
Aliaksei <alv96@bk.ru>
2019-12-05 11:20:36 +0300

    fix: update filex

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
         cssEase: 'linear'
     });
 });
+
+$(document).ready(function() {
+    $('.reviews-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
